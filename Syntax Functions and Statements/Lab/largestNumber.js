659141043518afaf47d64a10a583357f39af002c
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-12 19:35:04 +0300

    Largest Number Function

---
+++ Syntax Functions and Statements/Lab/largestNumber.js
@@ -0,0 +1,25 @@
+function largestNumber (numberOne, numberTwo, numberThree){
+    let result;
+    let numOne = numberOne;
+    let numTwo = numberTwo;
+    let numThree = numberThree;
+
+    if (numOne >= numTwo && numOne >= numThree) {
+        result = numOne;
+    }
+    else if (numTwo >= numOne && numTwo >= numThree){
+        result = numTwo;
+    }
+    else {
+
+        result = numThree;
+    }
+
+    console.log(`The largest number is ${result}.`)
+    }
+
+
+largestNumber(5, -3, 16)
+
+largestNumber(9, 11, 18)
+
