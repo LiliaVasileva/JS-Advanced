a61118108821c9e410c2c94073fe3b30203b30ea
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-26 22:32:56 +0300

    Adding Second and Third task

--- DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
-}
+    let ulElements = document.getElementById('items');
+    // взима текстовото съдържание и ги залепя//
+
+    let textAreaElement = document.getElementById('result');
+    textAreaElement.textContent = ulElements.textContent;
+
+};
--- DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
+++ DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,12 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstNumEl = document.getElementById('num1');
+    let secondNumEl = document.getElementById('num2');
+
+    let firstNumber = Number(firstNumEl.value);
+    let secondNumber = Number(secondNumEl.value);
+
+    let sum = firstNumber + secondNumber
+    let resultEl = document.getElementById('sum');
+    resultEl.value = sum;
+
 }
