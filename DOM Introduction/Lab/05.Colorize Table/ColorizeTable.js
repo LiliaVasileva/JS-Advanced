60c9b34333e8c99a741930046933c035bc14e7db
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-27 19:24:38 +0300

    Adding Colorize Table Task

--- DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,23 @@
+// First Solution with CSS Selector
+
+// function colorize() {
+//     let rowElements = document.querySelectorAll('tr:nth-of-type(2n) td');
+//     rowElements.forEach(x => {
+//         x.style.backgroundColor = 'teal'
+//     }
+//         );
+//     }
+
+// Second Solution with For cycle
 function colorize() {
-    // TODO
+
+    let rowElements = document.getElementsByTagName('tr');
+    let rows = Array.from(rowElements);
+
+    rows.forEach((x, i) => {
+        if (i % 2 != 0) {
+            x.style.backgroundColor = 'teal';
+        }
+        
+    });
 }
