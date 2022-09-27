18ce06dfee41a63709a671254e02e845fa4bdf0d
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-27 21:01:54 +0300

    Substraction Task

--- DOM Introduction/Exercise/01. Subtraction/subtract.js
+++ DOM Introduction/Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,12 @@
 function subtract() {
-    console.log('TODO:...');
+    // all in one
+    let firstNumber = Number(document.getElementById('firstNumber').value);
+// other  way to do it:
+    let secondNumber = document.getElementById('secondNumber');
+    let secondNumberAsNumber = Number(secondNumber.value);
+
+    let result = firstNumber - secondNumberAsNumber;
+
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = result;   
 }
