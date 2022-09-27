f590b4d379383d2c69acd0d9dc89c0676c426ac9
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-27 20:31:16 +0300

    Adding Extract Parenthesis Task

--- DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
+    let costElement = document.querySelectorAll('tr td:nth-of-type(2)');
+
+    let sum = Array.from(costElement).reduce( (a, x) => {
+        let currentValue = Number(x.textContent) || 0;
+        return a + currentValue;
+        },0);
+
+    let resultElement = document.getElementById('sum');
+    resultElement.textContent = sum;
 
 }
