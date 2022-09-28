ba7ef7c96a56fca19343e4295e6dd4235dd1acd0
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-28 20:35:13 +0300

    Search in List Task

--- DOM Introduction/Exercise/04. Search in List/search.js
+++ DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+   let userWord = document.getElementById('searchText').value;
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+
+   let result = document.getElementById('result');
+   let countResult = 0;
+
+   for (let town of towns){
+      if (town.textContent.includes(userWord)){
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration ='underline';
+         countResult++
+      }else{
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration ='none';
+      }
+
+   }
+
+   result.textContent = `${countResult} matches found`;
 }
