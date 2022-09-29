8e16f7866148bb2c2acb5c38550e7219af831d5a
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-29 20:28:46 +0300

    Format the Text Task

--- DOM Introduction/Exercise/06. Format the Text/solution.js
+++ DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  let inputElement = document.getElementById('input');
+  let textArr = inputElement.value.split('.').filter(s => s !== '');
+  let resultDiv = document.getElementById('output')
+
+
+  while (textArr.length > 0) {
+    let text = textArr.splice(0, 3).join('. ') + '.';
+
+    let p = document.createElement('p');
+    p.textContent = text;
+
+    resultDiv.appendChild(p);
+  }
+
 }
