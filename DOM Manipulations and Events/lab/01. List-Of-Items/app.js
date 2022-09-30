9005d3294d97ed2ebaae5dbeb9a2a70d9650b11b
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-30 21:36:01 +0300

    Delete from Table

--- DOM Manipulations and Events/lab/01. List-Of-Items/app.js
+++ DOM Manipulations and Events/lab/01. List-Of-Items/app.js
@@ -1,10 +1,9 @@
 function addItem() {
     let inputElement = document.getElementById('newItemText');
-    let liElement = document.createElement('li');
     let ulElement = document.getElementById('items');
-
-    liElement.textContent = inputElement.value;
-
-    ulElement.appendChild(liElement);
     
+    let liElement = document.createElement('li');
+    liElement.textContent = inputElement.value;
+    
+    ulElement.appendChild(liElement);    
 }
--- DOM Manipulations and Events/lab/02. Delete-From-Table/app.js
+++ DOM Manipulations and Events/lab/02. Delete-From-Table/app.js
@@ -1,3 +1,29 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emailInput = document.querySelector('input[name="email"]');
+    let emailCellElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    let resultElement = document.getElementById('result');
+
+    let emailElements = Array.from(emailCellElements);
+    let targetElement = emailElements.find(x => x.textContent == emailInput.value);
+
+    if(targetElement){
+        targetElement.parentNode.remove();
+       resultElement.textContent = 'Deleted.'; 
+    }else{
+        resultElement.textContent = 'Not found.';
+    }
 }
+
+    // Applies for NodeList and HTML Collection
+    // for (const tdElement of emailCellElements){
+    //     console.log(tdElement.textContent)
+    // }
+
+    // how to delete an Element :
+    // targetElement.parentNode.remove();
+
+    //  Applies only for NodeList
+    // let emailElements = Array.from(emailCellElements);
+
+    // old way of deleting 
+    // targetElement.parentNode.removeChild(targetElement)
