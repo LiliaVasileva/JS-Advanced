b1f3c2c3cfd8e8392d61143a137261a43006d1ac
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-09-30 20:37:50 +0300

    DOM Manipulations and Events

--- DOM Introduction/Exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction/Exercise/07. Hell's Kitchen/app.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      let input = JSON.parse.document.querySelector('#input textarea').value;
+      let avgSalary = 0;
+      let totalSalary = 0;
+      let currentAvgSalary = 0;
+      let bestName = '';
+      let output = {};
+
+      for (let line of input){
+         console.log(line);
+         let restaurantInfo = line.split(' -');
+      }
+
       
    }
 }
