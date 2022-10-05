2e650739a555f2c1e17d6eac8566701b106912d5
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-10-05 20:57:52 +0300

    Adding/Deleting Element Task

--- DOM Manipulations and Events/lab/03. Add-Delete/app.js
+++ DOM Manipulations and Events/lab/03. Add-Delete/app.js
@@ -1,3 +1,22 @@
 function addItem() {
-    //TODO...
+    let inputElement = document.getElementById('newItemText');
+    let ulElement = document.getElementById('items');
+    
+    let liElement = document.createElement('li');
+    liElement.textContent = inputElement.value;
+
+    // this is the way to clear input values
+    inputElement.value = '';
+
+    let linkElement = document.createElement('a')
+    linkElement.href = '#';
+    linkElement.textContent = '[Delete]';
+
+    linkElement.addEventListener('click', (e) =>{
+        e.currentTarget.parentElement.remove();
+    })
+    
+    liElement.appendChild(linkElement);
+    
+    ulElement.appendChild(liElement);    
 }
