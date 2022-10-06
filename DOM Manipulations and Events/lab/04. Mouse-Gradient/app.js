f3b37513e38a5dfaabb1bf5a5a223393170315c5
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-10-06 22:09:46 +0300

    Adding task Highlight -Active

--- DOM Manipulations and Events/lab/04. Mouse-Gradient/app.js
+++ DOM Manipulations and Events/lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement =document.getElementById('gradient')
+    let resultElement = document.getElementById('result')
+
+    const gradientMouseoverHandler = (e) => {
+        console.log(e.target.offsetWidth);
+        console.log(e.offsetX);
+        let percent =Math.floor((e.offsetX /e.target.offsetWidth) * 100);
+        resultElement.textContent = `${percent }%`;
+    }
+
+    gradientElement.addEventListener('mousemove',gradientMouseoverHandler )
+
 }
