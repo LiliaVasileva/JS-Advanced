436d218136de08581fca294a9d4f83790d695d10
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-10-08 16:07:38 +0300

    Sections Task

--- DOM Manipulations and Events/exercise/01. Sections/script.js
+++ DOM Manipulations and Events/exercise/01. Sections/script.js
@@ -1,3 +1,14 @@
 function create(words) {
-   console.log('TODO:...');
+   let existingDiv = document.getElementById('content');
+   for (let word of words){
+      let newDivElement = document.createElement('div');
+      let newParagraphElement = document.createElement('p')
+      newParagraphElement.textContent = word;
+      newParagraphElement.style.display = 'none';
+      newDivElement.appendChild(newParagraphElement);
+      newDivElement.addEventListener('click', (e) => {
+         e.target.childNodes[0].style.display ='initial';
+      });
+      existingDiv.appendChild(newDivElement);
+   };
 }
