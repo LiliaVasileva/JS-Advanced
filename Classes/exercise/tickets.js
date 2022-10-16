a6d093e05c90893ec3352e32fab312a31af56500
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-10-16 20:19:07 +0300

    Ticket Task

---
+++ Classes/exercise/tickets.js
@@ -0,0 +1,37 @@
+function ticketSorter(tickets, sorting){
+    class Ticket {
+        constructor(destination, price, status){
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+
+        }
+    }
+
+    let result = [];
+
+    tickets.map((el)=>{
+        let [destination, price, status] = el.split('|')
+        price = Number(price);
+        result.push(new Ticket(destination, price, status));
+        
+    });
+
+    return result.sort((a,b) => {
+        if(typeof a[sorting] === 'number'){
+            return a[sorting] - b[sorting];
+        }else {
+            return a[sorting].localeCompare(b[sorting]);
+        }
+    });
+}
+
+console.log(ticketSorter(['Philadelphia|94.20|available',
+
+'New York City|95.99|available',
+
+'New York City|95.99|sold',
+
+'Boston|126.20|departed'],
+
+'destination'));
