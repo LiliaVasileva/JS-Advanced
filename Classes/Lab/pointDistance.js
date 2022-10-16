57f32b51ddfe35e584f7a0cd4c02d7ebe38fd4f2
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-10-16 09:32:12 +0300

    Point Distance Task Solution

---
+++ Classes/Lab/pointDistance.js
@@ -0,0 +1,23 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+    }
+    static distance(firstPoint, secondPoint) {
+        let a = Math.abs(firstPoint.x - secondPoint.x);
+        let b = Math.abs(firstPoint.y - secondPoint.y);
+
+        let distance = Math.sqrt(a**2 + b**2);
+
+        return distance
+    }
+    }
+
+
+    let p1 = new Point(5, 5);
+    let p2 = new Point(9, 8);
+
+    let distance = Point.distance(p1, p2);
+
+    console.log (distance)
